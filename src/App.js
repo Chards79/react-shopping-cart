3f7c5ed93ef9f314b95d4d00dd488dde3d5e4be2
Eric Richards <esholundr@yahoo.com>
2019-10-07 21:25:32 -0600

    Fixed Nav component to use Context, MVP complete

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<CartContext.Provider value={{ cart }}>
+				<CartContext.Provider value={cart}>
 					<Navigation cart={cart} />
 					{/* Routes */}
 					<Route exact path="/" component={Products} />
-					<Route path="/cart" component={cart} />
+					<Route path="/cart" component={ShoppingCart} />
 				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
